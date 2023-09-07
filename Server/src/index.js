d6893534bc8ea1706e12359b1b3bf72408cebc13
danielor92 <danielospinar@gmail.com>
2023-09-07 15:00:32 -0500

    App Full Functional atm

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,25 +1,14 @@
 const http = require("http");
 const getCharById = require("./controllers/getCharById");
-const PORT = 3001;
-const data = require("../src/utils/data");
 
+const PORT = 3001;
 const server = http
 	.createServer((req, res) => {
 		res.setHeader("Access-Control-Allow-Origin", "*");
 		if (req.url.includes("/rickandmorty/character")) {
 			const params = req.url.split("/");
 			const id = parseInt(params[params.length - 1]);
-			//getCharById(res, id);
-			const character = data.find((character) => character.id === id);
-			if (character) {
-				res.writeHead(200, { "content-type": "application/json" });
-				const response = JSON.stringify(character);
-				res.end(response);
-				// res.status(200).contentType({ "content-type": "application/json" }).json();
-			} else {
-				res.writeHead(404, { "content-type": "text/plain" });
-				res.end("ID not found");
-			}
+			getCharById(res, id);
 		}
 	})
 	.listen(PORT, "localhost");
