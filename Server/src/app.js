c27e5d4c6b3a518dc1ff6449c85f30e7876f29c5
danielor92 <danielospinar@gmail.com>
2023-09-13 18:38:13 -0500

    changes on server

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const server = express();
 const morgan = require("morgan");
 const router = require("./routes/index");
-const PORT = 3001;
 
 server.use(express.json());
 server.use(morgan("dev"));
@@ -18,7 +17,4 @@ server.use((req, res, next) => {
 });
 
 server.use("/rickandmorty", router);
-
-// server.listen(PORT, () => console.log(`Server raised in port: ${PORT}`));
-
 module.exports = server;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,24 +1,4 @@
-// const express = require("express");
-// const server = express();
-// const morgan = require("morgan");
-// const router = require("./routes/index");
-const PORT = 3001;
-
-// server.use(express.json());
-// server.use(morgan("dev"));
-// server.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Credentials", "true");
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-// 	next();
-// });
-
-// server.use("/rickandmorty", router);
-
 const server = require("./app");
 
+const PORT = 3001;
 server.listen(PORT, () => console.log(`Server raised in port: ${PORT}`));
