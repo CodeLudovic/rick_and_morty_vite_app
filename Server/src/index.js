3ae955fbc02caf2779c09bf8dec3c37d0a2c3cd2
danielor92 <danielospinar@gmail.com>
2023-09-20 18:17:31 -0500

    Models Created, BD Connected

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,4 +1,7 @@
 const server = require("./app");
+const { conn } = require("./DB_connection");
 
 const PORT = 3001;
-server.listen(PORT, () => console.log(`Server raised in port: ${PORT}`));
+server.listen(PORT, () => {
+	conn.sync({ force: true }), console.log(`Server raised in port: ${PORT}`);
+});
